0cd932f9798b7e3b1121ced0d77048f48bc3ede1
Gaël <33gael.dev@gmail.com>
2026-05-13 11:44:43 +0200

    JSON file

--- src/ft_Sherlock.py
+++ src/ft_Sherlock.py
@@ -2,3 +2,6 @@ from bs4 import BeautifulSoup
 import asyncio
 import httpx
 from typing import Dict, Any
+
+async def   site_checker(client_name: httpx.AsyncClient, site_name: str, site_data: Dict[str, Any], username: str):
+    url = site_data["url"].format(username);
