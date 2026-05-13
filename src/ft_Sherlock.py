e43bef1389e9486b199feb6029c59e819cd4d22a
Gaël <33gael.dev@gmail.com>
2026-05-13 15:37:22 +0200

    Banner + Headers usage to bypass little protections

--- src/ft_Sherlock.py
+++ src/ft_Sherlock.py
@@ -3,15 +3,19 @@ import httpx
 from typing import Dict, Any
 
 async def site_checker(client_name: httpx.AsyncClient, site_name: str, site_data: str, username: str):
-	url = site_data.replace("{username}", username)
-	try:
-		reponse = await client_name.get(url, timeout = 10.0)
-		if reponse.status_code == 200 :
-			return {"site": site_name, "Found": True, "url": url}
-		else :
-			return {"site": site_name, "Found": False, "url": url}
-	except httpx.RequestError:
-		return {"site": site_name, "Found": False, "error": "timeout or dns error :("}
+    url = site_data.replace("{username}", username)
+    try:
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        }
+        response = await client_name.get(url, headers=headers, timeout=10.0, follow_redirects=True)
+        if response.status_code == 200:
+            return {"site": site_name, "Found": True, "url": url}
+        else:
+            return {"site": site_name, "Found": False, "url": url}
+            
+    except httpx.RequestError:
+        return {"site": site_name, "Found": False, "error": "timeout or dns error"}
 
 async def site_scanner(username: str, sites_dict: Dict[str, str]):
 	print(f"[*] - Searching an account with the Username : '{username}' on {len(sites_dict)} sites")
--- src/main.py
+++ src/main.py
@@ -4,8 +4,21 @@ import asyncio
 from pathlib import Path
 from rich.console import Console
 
+BANNER = r"""
+[bold cyan]
+  __ _             _____ _               _            _    
+ / _| |           / ____| |             | |          | |   
+| |_| |_         | (___ | |__   ___ _ __| | ___   ___| | __
+|  _| __|         \___ \| '_ \ / _ \ '__| |/ _ \ / __| |/ /
+| | | |_  ______  ____) | | | |  __/ |  | | (_) | (__|   < 
+|_|  \__||______||_____/|_| |_|\___|_|  |_|\___/ \___|_|\_\
+[italic white]                                                           v0.1[/italic white][/bold cyan]
+"""
+
 if __name__ == "__main__":
     console = Console()
+    console.print(BANNER)
+    console.print("-" * 65)
     dir = Path(__file__).parent
     file = dir / "sites.json"
     with open(file, "r") as file:
@@ -20,6 +33,6 @@ if __name__ == "__main__":
             elif "error" in res:
                 console.print(f"[bold dark_red][!] - Error in {res['site']} : {res['error']}[/bold dark_red]")
             else:
-                console.print(f"[bold dark_orange][-] - Error username not found in {res['site']}[/bold dark_orange]")
+                console.print(f"[bold dark_orange][-] - Error: username not found in {res['site']}[/bold dark_orange]")
     else:
         console.print(f"[bold red][X] - Please enter a valid username[/bold red]")
