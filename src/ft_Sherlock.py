3223b878f8096678ad8f3e6a49da6131a55065af
Gaël <33gael.dev@gmail.com>
2026-05-13 12:18:18 +0200

    Edit first function with async calls

--- src/ft_Sherlock.py
+++ src/ft_Sherlock.py
@@ -3,5 +3,18 @@ import asyncio
 import httpx
 from typing import Dict, Any
 
-async def   site_checker(client_name: httpx.AsyncClient, site_name: str, site_data: Dict[str, Any], username: str):
-    url = site_data["url"].format(username);
+async def site_checker(client_name: httpx.AsyncClient, site_name: str, site_data: Dict[str, Any], username: str):
+	url = site_data["url"].format(username)
+	try:
+		reponse = await client_name.get(url, timeout = 10.0)
+		if reponse.status_code == 200 :
+			return {"site": site_name, "Found": True, "url": url}
+		else :
+			return {"site": site_name, "Found": False, "url": url}
+	except httpx.RequestError:
+		return {"site": site_name, "Found": False, "error": "timeout or dns error :("}
+
+async def site_scanner(username: str, sites_dict: Dict[str, Any]):
+    print(f"[*] - Searching an account with the Username : '{username}' on {len(sites_dict)} sites")
+    async with httpx.AsyncClient as client:
+        
